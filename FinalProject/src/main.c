ffb126c4c9c6c2d726645e2408e1e262045186d8
Jordan Heemskerk <jheems@uvic.ca>
2014-10-07 17:04:29 -0700

    Playing around with configuring PA1 and ADC

--- FinalProject/src/main.c
+++ FinalProject/src/main.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include "diag/Trace.h"
+#include "cmsis/cmsis_device.h"
 
 // ----------------------------------------------------------------------------
 //
@@ -28,17 +29,57 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
+void initADC();
+void configurePA1();
+
 int
 main(int argc, char* argv[])
 {
   // At this stage the system clock should have already been configured
   // at high speed.
 
+  configurePA1();
+  initADC();
+
   // Infinite loop
   while (1)
     {
        // Add your code here.
     }
+}
+
+void configurePA1() {
+	/* Enable clock for GPIOA peripheral */
+	// Relevant register: RCC->AHBENR
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	/* Configure PA1 as analog */
+	// Relevant register: GPIOA->MODER
+	GPIOA->MODER |= GPIO_MODER_MODER1;
+
+	/* Ensure no pull-up/pull-down for PA1 */
+	// Relevant register: GPIOA->PUPDR
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+}
+
+void initADC() {
+
+	//enable ADC on APB
+	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
+
+	//set ADC clock source to asynchrous
+	ADC1->CFGR2 &= ~(0x3);
+
+	//Enable ADC
+	ADC1->CR |= ADC_CR_ADEN;
+	//ADC1->CR |= ADC_CR_ADCAL;
+
+	//Wait until ADRDY=1 so that ADC is ready
+	while(1) {
+		trace_printf("%d\n", ADC1->DR) ;
+	}
+
+	trace_printf("ADC is enabled\n");
 }
 
 #pragma GCC diagnostic pop
