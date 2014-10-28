a017bc3d7bf84d3930e59f9efe94e80814447ad1
jheems <jheems@uvic.ca>
2014-10-28 16:52:20 -0700

    ADC configuration and polling working

--- FinalProject/src/main.c
+++ FinalProject/src/main.c
@@ -30,36 +30,46 @@
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
 void initADC();
-void configurePA1();
+void initDAC();
+void configurePA0();
+unsigned int pollADC();
 
 int
-main(int argc, char* argv[])
-{
+main(int argc, char* argv[]) {
   // At this stage the system clock should have already been configured
   // at high speed.
 
-  configurePA1();
-  initADC();
+	configurePA0();
+	initADC();
+
 
   // Infinite loop
-  while (1)
-    {
-       // Add your code here.
-    }
+	while (1) {
+	  trace_printf("ADC Value: %d\n", pollADC());
+	   // Add your code here.
+	}
 }
 
-void configurePA1() {
+void configurePA0() {
 	/* Enable clock for GPIOA peripheral */
 	// Relevant register: RCC->AHBENR
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-	/* Configure PA1 as analog */
+	/* Configure PA0 as analog */
 	// Relevant register: GPIOA->MODER
-	GPIOA->MODER |= GPIO_MODER_MODER1;
+	GPIOA->MODER &= ~GPIO_MODER_MODER0;
 
-	/* Ensure no pull-up/pull-down for PA1 */
+	/* Ensure no pull-up/pull-down for PA0 */
 	// Relevant register: GPIOA->PUPDR
-	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR0);
+}
+
+void initDAC() {
+RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+
+
+
+
 }
 
 void initADC() {
@@ -68,18 +78,36 @@ void initADC() {
 	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
 
 	//set ADC clock source to asynchrous
-	ADC1->CFGR2 &= ~(0x3);
+	//ADC1->CFGR2 &= ~(0x3);
+
+	//Cal ADC
+	ADC1->CR = ADC_CR_ADCAL;
+	while (ADC1->CR == ADC_CR_ADCAL);
+	trace_printf("ADC is calibrated\n");
+
+
+	ADC1->CFGR1 |= (ADC_CFGR1_CONT | ADC_CFGR1_OVRMOD);
+
+	//Select Channel 0 for ADC
+	ADC1->CHSELR = ADC_CHSELR_CHSEL0;
+
 
 	//Enable ADC
 	ADC1->CR |= ADC_CR_ADEN;
-	//ADC1->CR |= ADC_CR_ADCAL;
-
-	//Wait until ADRDY=1 so that ADC is ready
-	while(1) {
-		trace_printf("%d\n", ADC1->DR) ;
-	}
-
+	while ((ADC1->ISR & 0x1) != 0x1);
 	trace_printf("ADC is enabled\n");
+
+
+}
+
+unsigned int pollADC() {
+
+	ADC1->CR |= ADC_CR_ADSTART;
+	while ((ADC1->ISR & ADC_ISR_EOC) == 0);
+	ADC1->ISR &= ~(ADC_ISR_EOC);
+	unsigned int val = ADC1->DR;
+	return val;
+
 }
 
 #pragma GCC diagnostic pop
